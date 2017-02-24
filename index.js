12254fa029517e96947edb24a0e37cc9cb561226
Nuno Miguel Soares Silva <nuno.miguel.silva@ua.pt>
2017-02-24 22:39:30 +0000

    maybe it's better like this

--- index.js
+++ index.js
@@ -2,23 +2,10 @@
  * Created by Nuno Silva on 24/02/2017.
  */
 
-function myFunction() {
-    myVar = setTimeout(showPage, 3000);
-}
+function setEventListeners() {
 
-function showPage() {
-    fade(document.getElementById("loader"));
-}
+    document.getElementById("monogram").onclick = function () {
+        document.getElementById("loader").style.display = "none";
+    };
 
-function fade(element) {
-    var op = 1;  // initial opacity
-    var timer = setInterval(function () {
-        if (op <= 0.1){
-            clearInterval(timer);
-            element.style.display = 'none';
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.1;
-    }, 50);
 }
