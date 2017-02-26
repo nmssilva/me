33a060c8a87847e2c26be1606368203bbf20afdf
Nuno Miguel Soares Silva <nuno.miguel.silva@ua.pt>
2017-02-26 22:15:44 +0000

    "about me" section

--- index.js
+++ index.js
@@ -2,10 +2,21 @@
  * Created by Nuno Silva on 24/02/2017.
  */
 
+function runSite() {
+
+    setEventListeners();
+}
+
 function setEventListeners() {
 
     document.getElementById("monogram").onclick = function () {
-        document.getElementById("loader").style.display = "none";
+        document.getElementById("loader").style.display = "none"; //hide loader
+        document.getElementById("monogramhead").style.visibility = "visible"; //make header appear
+        document.body.style.overflow = "visible"; //enable scrolling
+        window.scrollTo(0,0); // on top after click
     };
+
+
+
 
 }
